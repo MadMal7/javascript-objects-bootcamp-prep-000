aa38fbe23a89901b760e0333ba956e3d433fd2b6
Malachi Scott Prah <malachiprah@gmail.com>
2020-02-05 20:25:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,10 +1,16 @@
-var playlist = { Motorhead: 'Ace of Spades' };
+var playlist = {
+  'Motorhead': ['Ace of Spades', 'Overkill', 'Whorehouse Blues'],
+  'Black Sabbath': ['Black Sabbath', 'Paranoid', 'Sabbath Bloody Sabbath'],
+  'Megedeth': ['My Last Words', 'Holy Wars', 'In My Darkest Hour'],
+  'Mic Gordon': 'Rip & Tear'
+  }
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName];
-  playlist;
+  delete playlist[artistName]
+  return playlist
 }
